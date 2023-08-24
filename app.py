71aa56f14b726b5bc1b987aa02954266543c0808
xeven.ai <143064727+xevenai@users.noreply.github.com>
2023-08-24 12:05:24 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ warnings.filterwarnings('ignore')
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 icon = Image.open('skin.jpg')
-st.set_page_config(page_title='EfficientSkinDis', page_icon = icon)
-st.header('EfficientSkinDis WebServer')
+st.set_page_config(page_title='EfficientSkinDis-xeven', page_icon = icon)
+st.header('Skin Disease Detection ')
 
 #Load Model
 def effNetb2():

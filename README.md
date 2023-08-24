05617c3b61a70145b78dd40407be47c1099cb31a
xeven.ai <143064727+xevenai@users.noreply.github.com>
2023-08-24 11:31:54 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Skin-Disease-Setection-using-EfficientNET
+# EfficientSkinDis
+
+[![PyTorch](https://img.shields.io/badge/Framework-PyTorch-79FEE1)](https://pytorch.org)
+![Deep Learning](https://img.shields.io/badge/Type-Deep%20Learning-FFAE42)
+[![Language](https://img.shields.io/badge/Language-Python-0000FF)](https://www.python.org/)
+[![Model](https://img.shields.io/badge/Model-EfficientNet%20B2-79FFB2)](https://pytorch.org/vision/main/models/generated/torchvision.models.efficientnet_b2.html)
+
+## Web-Server
+This repo contains source coude for *EfficientSkinDis* webserver, developed and deployed using Streamlit, which can be accessed on [![Web-Server](https://img.shields.io/badge/WebServer-Streamlit-808080)](https://abdulrafay97-efficientskindis-app-ooncon.streamlit.app/)
+
+## How To use the Web-Server
+1) Open the webserver using the above link.
+2) Upload a picture of a disease in the file uploader.
+3) You will get the predicted result in 1/2 seconds.
+
+## Dataset
+The dataset is available to access the 80:20 split on the [![Dataset](https://img.shields.io/badge/Dataset-Drive-FFFFF)](https://drive.google.com/drive/folders/1AiDVpgy-o4ZLKXZ_yqnWWEHFYhCbrfP1?usp=share_link)
+
+### Sample-Dataset
+![](Sample-Images/Capture.PNG)
---
+++ app.py
@@ -0,0 +1,83 @@
+#Import Packages
+import numpy as np
+from PIL import Image
+import torch
+from torch.nn import functional as F
+from torchvision import models, transforms
+import torch.nn as nn
+import streamlit as st
+import warnings
+warnings.filterwarnings('ignore')
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+icon = Image.open('skin.jpg')
+st.set_page_config(page_title='EfficientSkinDis', page_icon = icon)
+st.header('EfficientSkinDis WebServer')
+
+#Load Model
+def effNetb2():
+    model = models.efficientnet_b2(pretrained=False).to(device)
+    in_features = 1024
+    model._fc = nn.Sequential(
+        nn.BatchNorm1d(num_features=in_features),    
+        nn.Linear(in_features, 512),
+        nn.ReLU(),
+        nn.BatchNorm1d(512),
+        nn.Linear(512, 128),
+        nn.ReLU(),
+        nn.BatchNorm1d(num_features=128),
+        nn.Dropout(0.4),
+        nn.Linear(128, 31),).to(device)
+
+    model.load_state_dict(torch.load('Weights.h5' , map_location=torch.device('cpu')) )
+    model.eval()
+
+    return model
+
+#Calculating Prediction
+def Predict(img):
+    allClasses = ['Actinic Keratosis', 'Basal Cell Carcinoma',
+	      'Dariers', 'Dermatofibroma',
+	      'Epidermolysis Bullosa Pruriginosa', 'Hailey-Hailey Disease',
+	      'Herpes Simplex', 'Impetigo', 'Larva Migrans', 'Leprosy Borderline',
+	      'Leprosy Lepromatous', 'Leprosy Tuberculoid', 'Lichen Planus',
+	      'Lupus Erythematosus Chronicus Discoides', 'Melanoma',
+	      'Molluscum Contagiosum', 'Mycosis Fungoides', 'Neurofibromatosis',
+	      'Nevus', 'Papilomatosis Confluentes And Reticulate', 'Pediculosis Capitis',
+	      'Pigmented Benign Keratosis', 'Pityriasis Rosea', 'Porokeratosis Actinic',
+	      'Psoriasis', 'Seborrheic Keratosis', 'Squamous Cell Carcinoma', 'Tinea Corporis',
+	      'Tinea Nigra', 'Tungiasis', 'Vascular Lesion']
+    Mod = effNetb2()
+    out = Mod(img)
+    _, predicted = torch.max(out.data, 1)
+    allClasses.sort()
+    labelPred = allClasses[predicted]
+    return labelPred
+
+
+
+#Get Image
+file_up = st.file_uploader('Upload an Image', type = "jpg")
+
+#Normalizing
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+
+#Transforming the Image
+data_transform = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                normalize
+            ])
+
+if file_up is not None:
+    # display image that user uploaded
+    image = Image.open(file_up)
+    st.image(image, caption = 'Uploaded Image.', use_column_width = True)
+    st.write("")
+    st.write("Just a second ...")
+    img = data_transform(image)
+    img = torch.reshape(img , (1, 3, 224, 224))
+    prob = Predict(img)
+    st.write(prob)
